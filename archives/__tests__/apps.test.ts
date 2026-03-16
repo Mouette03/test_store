283130f81a286bf077967e73826373690266ada9
Damien <mouette03@yahoo.fr>
2026-03-16 20:30:45 +0100

    test

--- archives/__tests__/apps.test.ts
+++ archives/__tests__/apps.test.ts
@@ -3,7 +3,6 @@ import { appInfoSchema, dynamicComposeSchema } from '@runtipi/common/schemas'
 import { fromError } from 'zod-validation-error';
 import fs from 'node:fs'
 import path from 'node:path'
-import { type } from "arktype";
 
 const getApps = async () => {
   const appsDir = await fs.promises.readdir(path.join(process.cwd(), 'apps'))
@@ -47,14 +46,14 @@ describe("each app should have a valid config.json", async () => {
   for (const app of apps) {
     test(`app ${app} should have a valid config.json`, async () => {
       const fileContent = await getFile(app, 'config.json')
-      const parsed = appInfoSchema.omit('urn')(JSON.parse(fileContent || '{}'))
+      const parsed = appInfoSchema.omit({ urn: true }).safeParse(JSON.parse(fileContent || '{}'))
 
-      if (parsed instanceof type.errors) {
-        const validationError = fromError(parsed.summary);
+      if (!parsed.success) {
+        const validationError = fromError(parsed.error);
         console.error(`Error parsing config.json for app ${app}:`, validationError.toString());
       }
 
-      expect(parsed).toBeDefined()
+      expect(parsed.success).toBe(true)
     })
   }
 })
@@ -65,14 +64,14 @@ describe("each app should have a valid docker-compose.json", async () => {
   for (const app of apps) {
     test(`app ${app} should have a valid docker-compose.json`, async () => {
       const fileContent = await getFile(app, 'docker-compose.json')
-      const parsed = dynamicComposeSchema(JSON.parse(fileContent || '{}'))
+      const parsed = dynamicComposeSchema.safeParse(JSON.parse(fileContent || '{}'))
 
-      if ( parsed  instanceof type.errors) {
-        const validationError = fromError(parsed);
+      if (!parsed.success) {
+        const validationError = fromError(parsed.error);
         console.error(`Error parsing docker-compose.json for app ${app}:`, validationError.toString());
       }
 
-      expect(parsed).toBeDefined()
+      expect(parsed.success).toBe(true)
     })
   }
 });
